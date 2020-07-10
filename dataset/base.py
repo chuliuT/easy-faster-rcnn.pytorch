bf6d5b80948b952d1db52217dbb2798c92cb0ccf
ethon <iloveitre@gmail.com>
2020-07-10 22:03:47 +0800

    anno dataset/base.py

--- dataset/base.py
+++ dataset/base.py
@@ -10,18 +10,19 @@ from torch import Tensor
 from torch.nn import functional as F
 from torchvision.transforms import transforms
 
-
+#数据集的基类，继承自torch的 Dataset类
 class Base(torch.utils.data.dataset.Dataset):
 
-    class Mode(Enum):
-        TRAIN = 'train'
-        EVAL = 'eval'
-
+    class Mode(Enum):#Mode为枚举类型
+        TRAIN = 'train'#训练模式
+        EVAL = 'eval'#验证模式
+    #数据集的类型
     OPTIONS = ['voc2007', 'coco2017', 'voc2007-cat-dog', 'coco2017-person', 'coco2017-car', 'coco2017-animal']
-
+    #一个静态方法，返回的类型为 Base类 的子类
     @staticmethod
     def from_name(name: str) -> Type['Base']:
         if name == 'voc2007':
+            #这里的dataset为 package的dataset  不要跟上面的 torch的dataset混淆了
             from dataset.voc2007 import VOC2007
             return VOC2007
         elif name == 'coco2017':
@@ -43,38 +44,39 @@ class Base(torch.utils.data.dataset.Dataset):
             raise ValueError
 
     def __init__(self, path_to_data_dir: str, mode: Mode, image_min_side: float, image_max_side: float):
-        self._path_to_data_dir = path_to_data_dir
-        self._mode = mode
-        self._image_min_side = image_min_side
-        self._image_max_side = image_max_side
+        self._path_to_data_dir = path_to_data_dir# 数据集的路径
+        self._mode = mode # 模式
+        self._image_min_side = image_min_side#短边
+        self._image_max_side = image_max_side#长边
 
-    def __len__(self) -> int:
+    def __len__(self) -> int:#继承torch.utils.data.dataset.Dataset 必须实现的方法
         raise NotImplementedError
-
+    # 定义一个获取 图像 image_id, image, scale, bboxes, labels 在子类里面实现
     def __getitem__(self, index: int) -> Tuple[str, Tensor, Tensor, Tensor, Tensor]:
         raise NotImplementedError
-
+    # 计算 mAP 和 每个类的AP
     def evaluate(self, path_to_results_dir: str, image_ids: List[str], bboxes: List[List[float]], classes: List[int], probs: List[float]) -> Tuple[float, str]:
         raise NotImplementedError
-
+    # 将测试的结果  写入 txt 的文件里
     def _write_results(self, path_to_results_dir: str, image_ids: List[str], bboxes: List[List[float]], classes: List[int], probs: List[float]):
         raise NotImplementedError
 
     @property
-    def image_ratios(self) -> List[float]:
+    def image_ratios(self) -> List[float]:# 返回图像的宽/高的比例
         raise NotImplementedError
 
     @staticmethod
-    def num_classes() -> int:
+    def num_classes() -> int:# 数据集的类别
         raise NotImplementedError
 
+    # 用于图像的  短边和长边的处理  短边缩短为 image_min_side  长边：image_max_side
     @staticmethod
     def preprocess(image: PIL.Image.Image, image_min_side: float, image_max_side: float) -> Tuple[Tensor, float]:
         # resize according to the rules:
         #   1. scale shorter side to IMAGE_MIN_SIDE
         #   2. after scaling, if longer side > IMAGE_MAX_SIDE, scale longer side to IMAGE_MAX_SIDE
-        scale_for_shorter_side = image_min_side / min(image.width, image.height)
-        longer_side_after_scaling = max(image.width, image.height) * scale_for_shorter_side
+        scale_for_shorter_side = image_min_side / min(image.width, image.height)# 短边的尺度缩放因子  image_min_side：600
+        longer_side_after_scaling = max(image.width, image.height) * scale_for_shorter_side #长边的缩放因子
         scale_for_longer_side = (image_max_side / longer_side_after_scaling) if longer_side_after_scaling > image_max_side else 1
         scale = scale_for_shorter_side * scale_for_longer_side
 
